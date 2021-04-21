a2e5d79f3f4cac96bf6b45964b87d2402bdb7c14
acurilins <68602525+acurilins@users.noreply.github.com>
2021-04-21 20:40:01 +0400

    Update lesson_5.6.py

--- lesson_5/lesson_5.6.py
+++ lesson_5/lesson_5.6.py
@@ -1,1 +1,18 @@
-ECHO is on.
+my_file = "lesson_5.6.txt"
+subjects = {}
+
+try:
+    with open(my_file, 'r') as fh:
+        for line in fh.readlines():
+            data = line.replace('(', ' ').split()
+
+            subjects[data[0][:-1]] = sum(
+                int(i) for i in data if i.isdigit()
+            )
+except IOError as e:
+    print(e)
+except ValueError:
+    print("Error")
+
+print(subjects)
+
