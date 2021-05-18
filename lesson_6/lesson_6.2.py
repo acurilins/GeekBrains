f84b8147711e9adf69dde466645d66c06053c2e2
acurilins <68602525+acurilins@users.noreply.github.com>
2021-05-18 21:44:16 +0400

    Update lesson_6.2.py

--- lesson_6/lesson_6.2.py
+++ lesson_6/lesson_6.2.py
@@ -1,1 +1,38 @@
-6.2
+class Road:
+    __length = None
+    __width = None
+    weigth = None
+    tickness = None
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+        print('Creat road_to_village object')
+
+    def intake(self):
+        self.weigth = 25
+        self.tickness = 0.05
+        intake = self.length * self.width * self.weigth * self.tickness / 1000
+        print(f'Need {intake} ton for the building')
+
+road_to_village = Road(20000, 6)
+road_to_village.intake()
+
+-------------------------------------------------------------------------------------------
+
+class Road:
+    def __init__(self, _length, _width):
+        self._length = _length
+        self._width = _width
+
+    def mass(self):
+        return self._length * self._width
+
+
+class MassCount(Road):
+    def __init__(self, _length, _width, volume):
+        super().__init__(_length, _width)
+        self.volume = volume
+
+
+r = MassCount(25, 10000, 125)
+print(r.mass())
