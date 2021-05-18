c1817397ddfe1b9c22e06c153bc48c69e248247f
acurilins <68602525+acurilins@users.noreply.github.com>
2021-05-18 21:48:28 +0400

    Update lesson_6.5.py

--- lesson_6/lesson_6.5.py
+++ lesson_6/lesson_6.5.py
@@ -1,1 +1,61 @@
-6.5
+class Stationery:
+    atr_title = 'Title'
+    def draw(self):
+        print('Запуск отрисовки.')
+class Pen(Stationery):
+    def draw(self):
+        print('Отрисовка ручкой')
+class Pencil(Stationery):
+    def draw(self):
+        print('Отрисовка карандашом')
+class Handle(Stationery):
+    def draw(self):
+        print('Отрисовка маркером')
+
+my_pen = Pen()
+my_pencil = Pencil()
+my_handle = Handle()
+my_pen.draw()
+my_pencil.draw()
+my_handle.draw()
+
+_____________________________________________________________________________
+
+class Stationary:
+    def __init__(self, title):
+        self.title = title
+
+    def draw(self):
+        return f'Запуск отрисовки {self.title}'
+
+
+class Pen(Stationary):
+    def __init__(self, title):
+        super().__init__(title)
+
+    def draw(self):
+        return f'Вы взяли {self.title}. Запуск отрисовки ручкой'
+
+
+class Pencil(Stationary):
+    def __init__(self, title):
+        super().__init__(title)
+
+    def draw(self):
+        return f'Вы взяли {self.title}. Запуск отрисовки карандашом'
+
+
+class Handle(Stationary):
+    def __init__(self, title):
+        super().__init__(title)
+
+    def draw(self):
+        return f'Вы взяли {self.title}. Запуск отрисовки маркером'
+
+
+pen = Pen('Ручка')
+pencil = Pencil('Карандаш')
+handle = Handle('Маркер')
+print(pen.draw())
+print(pencil.draw())
+print(handle.draw())
