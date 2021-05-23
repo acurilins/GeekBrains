6f4372cab8a2db1f327d5e0b58742c15b530c46d
acurilins <68602525+acurilins@users.noreply.github.com>
2021-05-23 21:41:42 +0400

    Update lesson_6.3.py

--- lesson_6/lesson_6.3.py
+++ lesson_6/lesson_6.3.py
@@ -1,32 +1,4 @@
 class Worker:
-    name = None
-    surname = None
-    position = None
-    profit = None
-    bonus = None
-
-    def __init__(self, name, surname, position, profit, bonus):
-        self.name = name
-        self.surname = surname
-        self.position = position
-        self.profit = profit
-        self.bonus = bonus
-
-class Position(Worker):
-    def __init__(self, name, surname, position, profit, bonus):
-        super().__init__(name, surname, position, profit, bonus)
-    def get_full_name(self):
-        return self.name + self.surname
-    def get_full_profit(self):
-        self.__income = {'profit': self.profit, 'bonus': self.bonus}
-        return self.__income
-
-manager = Position('Petr', 'Petrov', 'manager', 500, 100)
-print(manager.get_full_name(), manager.get_full_profit())
-
-____________________________________________________________________
-
-class Worker:
 
     def __init__(self, name, surname, position, wage, bonus):
         self.name = name
@@ -45,10 +17,9 @@ class Position(Worker):
 
     def get_total_income(self):
         return self._income.get('wage') + self._income.get('bonus')
-        # return f'{sum(self._income.values())}'
 
 
-a = Position('Peter', 'The Great', 'Beekeeper', 100000, 25000)
-print(a.get_full_name())
+a = Position('John', 'Wick', 'Equalizer', 4000, 1500)
+print(a.get_full_name(),)
 print(a.position)
-print(a.get_total_income())
+print(a.get_total_income(), "$")
