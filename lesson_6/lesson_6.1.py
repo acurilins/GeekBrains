320158b671318039d84f571a95031ac878ae893b
acurilins <68602525+acurilins@users.noreply.github.com>
2021-05-23 16:42:27 +0400

    Update lesson_6.1.py

--- lesson_6/lesson_6.1.py
+++ lesson_6/lesson_6.1.py
@@ -1,34 +1,13 @@
-import time
-class TrafficLight:
-    _color = None
-    _colors = ['red', 'yellow', 'green']
-
-    def __init__(self):
-        self._color = self._colors[0]
-
-    def running(self):
-        i=0
-        while i<5:
-            for el in TrafficLight._colors :
-                print(el)
-                i+=1
-                time.sleep(1)
-
-traffic = TrafficLight()
-traffic.running()
-
-----------------------------------------------------------------------------
-
 from time import sleep
 
 
 class TrafficLight:
-    __color = ['Красный', 'Желтый', 'Зеленый']
+    __color = ['Red', 'Yellow', 'Green']
 
     def running(self):
         i = 0
         while i < 3:
-            print(f'Светофор переключается \n '
+            print(f'Traffic Lights has changed the color to: \n '
                   f'{TrafficLight.__color[i]}')
             if i == 0:
                 sleep(7)
